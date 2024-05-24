84c68c5447d6b6e700a76e40253a2205f8f33e35
Erik <rainer.erik70@gmail.com>
2024-05-24 10:49:10 -0700

    Create the nav logic to change the pages when selected

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -4,9 +4,34 @@ import Header from "./components/UI/Header"
 import Nav from "./components/UI/Nav"
 import Project from "./components/UI/Project"
 import Container from "./UI/Container"
-
+import Home from "./UI/Home"
+import About from "./UI/About"
 const PortfolioContainer = () => {
- 
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+          return <Home />;
+        }
+        if (currentPage === 'Project') {
+          return <Project />;
+        }
+        if (currentPage === 'ContactForm') {
+          return <Form />;
+        }
+        return <About />;
+      };
+      
+      const handlePageChange = (page) => setCurrentPage(page);
+
+      return (
+        <div>
+          <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+          <main className="mx-3">{renderPage()}</main>
+        </div>
+    
+      );
+      
 };
 
 export default PortfolioContainer;
--- src/components/UI/Nav/index.jsx
+++ src/components/UI/Nav/index.jsx
@@ -1,2 +1,53 @@
 import { useState } from 'react';
 import './style.css';
+
+function NavTabs({ currentPage, handlePageChange }) {
+    return (
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+  
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          >
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#Project"
+            onClick={() => handlePageChange('Project')}
+            //  TODO: Add a comment explaining what this logic is doing
+  
+            className={currentPage === 'Project' ? 'nav-link active' : 'nav-link'}
+          >
+            Project
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#ContactForm"
+            onClick={() => handlePageChange('ContactForm')}
+  
+            className={currentPage === 'ContactForm' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact Me
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#About"
+            //  TODO: Add a comment explaining what this logic is doing
+  
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About Me
+          </a>
+        </li>
+      </ul>
+    );
+  }
+  
+  export default NavTabs;
