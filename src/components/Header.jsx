9530722a48e6d7803ab9c5be1cd29dcd44370855
Erik <rainer.erik70@gmail.com>
2024-05-24 13:36:48 -0700

    added the Github button that i will use in my header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import { FaGithub } from 'react-icons/fa';
+
 function Header(){
 return(
 <div>
 <h1>Erik Gontijo</h1>
+<a href="https://github.com/Erikrainer" target="_blank" rel="noopener noreferrer">
+      <FaGithub size={40} style={{ color: '#000' }} />
+</a>
 </div>
 )
 }
