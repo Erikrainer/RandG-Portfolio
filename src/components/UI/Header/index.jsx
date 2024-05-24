ff2e17a92f781f798f5c08fc62517526350b4b06
Erik <rainer.erik70@gmail.com>
2024-05-24 11:40:54 -0700

    Header done, fontawesome added to package.json

--- src/components/UI/Header/index.jsx
+++ src/components/UI/Header/index.jsx
@@ -1,11 +1,17 @@
 import './style.css';
 import Nav from "../Nav"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Header(){
 <div>
-<h1></h1>
+<h1>Erik Gontijo</h1>
 <Nav />
-<h3></h3>
+    <div className="App">
+        <a href="https://github.com/Erikrainer" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+    </div>
 </div>
 }
 
