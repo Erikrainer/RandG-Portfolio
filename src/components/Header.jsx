5eeb6147357f825ab7ad25fe112a192e178ba876
Erik <rainer.erik70@gmail.com>
2024-05-26 09:47:16 -0700

    it looks finished the home page and the contact me page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import "./style.css"
 function NavTabs() {
   const currentPage = useLocation().pathname;
   return (
-    <nav className="navbar navbar-expand-lg border-info border-bottom border-1">
-      <div className="collapse navbar-collapse justify-content-between">
+    <nav className="navbar-expand-sm border-info border-bottom border-1" id="navBar">
+      <div className="navbar-collapse justify-content-between">
     <h1 className="navbar-brand m-3 fw-bold text-uppercase" id="headerTitle">Erik Gontijo</h1>
     <ul className="navbar-nav">
       <li className="nav-item m-5" id="icons">
