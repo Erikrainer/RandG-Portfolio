0db65b68385b91d3237344dd7953d95155d2609f
Rainer <rainer.erik70@gmail.com>
2024-12-08 22:28:20 -0800

    new files created, new page created

---
+++ src/pages/UnrealProjectPage.jsx
@@ -0,0 +1,195 @@
+import { FaGithub, FaLink, FaVideo} from 'react-icons/fa';
+import React, { useState } from 'react';
+import ProjectCard from "../Utils/SmallProjectCard";
+import BigProjectCard from '../Utils/BigProjectCard';
+import "./UnrealProjectPage.css"
+function ProjectPage() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Function to handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Function to render cards based on current page
+  const renderCards = () => {
+    switch (currentPage) {
+      case 1:
+        return (
+          <>
+            {/* Render first set of cards */}
+            <BigProjectCard
+              title="Demo Game"
+              imageName="DemoGame.png"
+              description="For my course, I created my first game using Unreal Engine 5. It’s a simple project designed to demonstrate the engine's basic features. In the game, players can move, collect orbs, and face challenges like falling off the map or taking damage, which results in a game over.
+The main objective is to collect all the orbs to unlock a door and progress. The game includes environmental ambient sounds, sound effects, and background music with triggered changes based on player actions. It also features essential mechanics like restarting the game, quitting, and basic physics for interactions."
+              siteLink="https://1drv.ms/u/s!AhObPZJDkiIE1xJGKIs1eltXlcGn?e=FgOVTp"
+              githubLink="https://github.com/Erikrainer"
+            />
+            {/* <div className="row">
+        {currentPage === 1 && (
+          <>
+            <ProjectCard 
+              title="RandGBetterTextEditor"
+              imageName="RandGBetterTextEditor.png"
+              siteLink="https://randgbettertexteditor.onrender.com/"
+              githubLink="https://github.com/Erikrainer/RandGBetterTextEditor"
+            />
+            <ProjectCard 
+              title="RandG SocialNetwork"
+              imageName="RandG-SocialNetwork.png"
+              siteVideo="https://www.youtube.com/watch?v=9N03P2cJ-4g"
+              githubLink=" https://github.com/Erikrainer/RandG-SocialNetwork"
+            />
+            <ProjectCard 
+              title="RandG HexValue"
+              imageName="RandGHexValue.png"
+              githubLink="https://gist.github.com/Erikrainer/c5d9cd09d4a4a6087f4bee6b27d56f6d"
+            />
+            <ProjectCard 
+              title="RandG BlogPost"
+              imageName="RandGBlogpost.png"
+              siteLink=" https://randg-blogpost.onrender.com/"
+              githubLink="https://github.com/Erikrainer/RandG-BlogPost"
+            />
+            <ProjectCard 
+              title="RandG E commerce"
+              imageName="RandGECommerce.png"
+              siteVideo="https://youtu.be/nbOJKyJZl-A"
+              githubLink="https://github.com/Erikrainer/RandG-E-commerce"
+            />
+            <ProjectCard 
+              title="RandGCompany"
+              imageName="RandGCompany.png"
+              siteVideo="https://youtu.be/ODy1kA3qj7U"
+              githubLink="https://github.com/Erikrainer/RandGCompany"
+            />
+            <ProjectCard 
+              title="backEndNoteTaker"
+              imageName="backEndNoteTaker.png"
+              siteLink="https://backendnotetaker.onrender.com/"
+              githubLink="https://github.com/Erikrainer/backEndNoteTaker"
+            />
+            <ProjectCard 
+              title="LogoCreator"
+              imageName="logoCreator.gif"
+              siteVideo="https://www.youtube.com/watch?v=JKCg0m2nvX0"
+              githubLink="https://github.com/Erikrainer/logoCreator"
+            />    
+            <ProjectCard 
+              title="readmeAutomator"
+              imageName="readmeAutomator.gif"
+              siteVideo="https://www.youtube.com/watch?v=R0VuPYHrhj8"
+              githubLink="https://github.com/Erikrainer/readmeAutomator"
+            />     
+          </>
+        )}
+      </div>
+          </>
+        );
+      case 2:
+        return (
+          <>
+          {/* Render second set of cards */}
+            {/* <BigProjectCard
+              title="Gifs and Giggles"
+              imageName="gifs-n-giggles.gif"
+              description="This is a webapp that allows users to choose a genre of jokes and gifs to be displayed. The user can choose to display a different gif by selecting the corresponding button below the Gif. The modal allows the user to choose between pun, christmas, programming, spooky, and misc."
+              siteLink="https://ccreed86.github.io/gifs-n-giggles/"
+              githubLink="https://github.com/ccreed86/gifs-n-giggles"
+            />
+            <div className="row">
+        {currentPage === 2 && (
+          <>
+            <ProjectCard 
+              title="RainerWeatherDashboard"
+              imageName="RainerWeatherDashboard.gif"
+              siteLink="https://erikrainer.github.io/rainerWeatherDashboard/"
+              githubLink="https://github.com/Erikrainer/rainerWeatherDashboard"
+            />
+            <ProjectCard 
+              title="ProjectManagerAPI"
+              imageName="TaskBoard.gif"
+              siteLink="https://erikrainer.github.io/projectManagerAPI/"
+              githubLink="https://github.com/Erikrainer/projectManagerAPILinks"
+            />
+            <ProjectCard 
+              title="CaffeCodeBlog"
+              imageName="cafeCodeBlog.gif"
+              siteLink="https://erikrainer.github.io/caffeCodeBlog/"
+              githubLink="https://github.com/Erikrainer/caffeCodeBlog"
+            />
+            <ProjectCard 
+              title="PayrollTracker"
+              imageName="EmployeePayrollTracker.gif"
+              siteLink="https://erikrainer.github.io/payrollTracker/"
+              githubLink="https://github.com/Erikrainer/payrollTracker"
+            />
+            <ProjectCard 
+              title="Erik Rainer ShowCase"
+              imageName="erikrainerShowcase.gif"
+              siteLink="https://erikrainer.github.io/erikrainer-showcase/"
+              githubLink="https://github.com/Erikrainer/erikrainer-showcase"
+            />
+            <ProjectCard 
+              title="Horiseon Challenge"
+              imageName="horiseon.png"
+              siteLink="https://erikrainer.github.io/horiseon-challenge/"
+              githubLink="https://github.com/Erikrainer/horiseon-challenge"
+            />
+            <ProjectCard 
+              title="Prework Study Guide"
+              imageName="prework.png"
+              siteLink="https://erikrainer.github.io/prework-study-guide/"
+              githubLink="https://github.com/Erikrainer/prework-study-guide"
+            />
+          </>
+        )}
+      </div> */}
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <main className="main-container">
+      {renderCards()}
+
+      {/* Pagination */}
+      {/* <nav>
+      <ul className="pagination pagination-lg">
+        <li className="page-item">
+          <button
+            className="page-link"
+            disabled={currentPage === 1}
+            onClick={() => handlePageChange(currentPage - 1)}
+          >
+            <span aria-hidden="true">&laquo;</span>
+          </button>
+        </li>
+        <li className={`page-item ${currentPage === 1 ? 'active' : ''}`}>
+          <button className="page-link" onClick={() => handlePageChange(1)}>1</button>
+        </li>
+        <li className={`page-item ${currentPage === 2 ? 'active' : ''}`}>
+          <button className="page-link" onClick={() => handlePageChange(2)}>2</button>
+        </li>
+        <li className="page-item">
+          <button 
+            className="page-link"
+            disabled={currentPage === 2}
+            onClick={() => handlePageChange(currentPage + 1)}
+          >
+            <span aria-hidden="true">&raquo;</span>
+          </button>
+        </li>
+      </ul>
+    </nav> */}
+    </main>
+
+    
+  );
+}
+
+export default ProjectPage;
